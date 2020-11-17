2857cf8658157149711da76d88fc092361084f89
Thomas <tnp94@yahoo.com>
2020-11-17 12:01:37 -0800

    Added pug templating for the routes list view and the problem information view

--- HuecoTanks.js
+++ HuecoTanks.js
@@ -2,20 +2,35 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
 app.use(express.static('public'));
+// const url; // TODO set API url here
+const fetch = require('node-fetch');
+
 
 app.get('/', (req, res) => {
 
 });
 
-app.get('/map', (req, res) => {
-   
 
+app.get('/:route?', (req, res) => {
+   // Fetch the available problems at the route location and link to each problem
+   let problems = [];
+   res.render('route', {
+      route: `${req.params.route}`,
+      problemList: problems
+   });
 });
 
-app.get('/routes', (req, res) => {
-   
-
+app.get('/:route/:problem', (req, res) => {
+   // Fetch all information for the problem and display the information with the route pug template
+   res.render('problem', {
+      route: `<a href='/${req.params.route}'>${req.params.route}</a>`,
+      problem: `${req.params.problem}`
+      // Put other relevant information here
+   });
 });
 
 app.listen(port, () => {
