a5f2d2becc356b59407db796970ae23abfec1ee1
Thomas <tnp94@yahoo.com>
2020-11-19 11:42:12 -0800

    Fixed terminology, changing the word 'route' for 'location' to reduce confusion. Also made the locationList page display possible locations found

--- HuecoTanks.js
+++ HuecoTanks.js
@@ -6,7 +6,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
-// const url; // TODO set API url here
 
 // Fetch variables
 const fetch = require('node-fetch');
@@ -14,9 +13,10 @@ var minGrade = "V3";
 var maxGrade = "V3";
 const key = "200976075-5e0eef9985cd16b8e7a3f68105cd6b29";
 var maxResults = 5;
+const url = `https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=31.93&lon=-106.05&maxDistance=1&minDiff=${minGrade}&maxDiff=${maxGrade}&maxResults=${maxResults}&key=${key}`; // TODO set API url here
 
 let routes;
-fetch(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=31.93&lon=-106.05&maxDistance=1&minDiff=${minGrade}&maxDiff=${maxGrade}&maxResults=${maxResults}&key=${key}`)
+fetch(url)
     .then (response => {
         return response.json();
     })
@@ -30,30 +30,57 @@ app.get('/', (req, res) => {
 
 });
 
-app.get('/routes', (req, res) => {
+app.get('/locations', (req, res) => {
    // Fetch the available routes at location and link to each problem
-   let routesArr = []; // Each route should link to the route page
+   let locationsArr= []; // Each route should link to the route page
+   let locations = {};
 
    // Simple test case, tested and works (11/18/2020)
-   routesArr[0] = routes.routes[0].name;
-   res.render('routesList', {
-      routesList: `${routesArr}`
+   routes.routes.forEach(route => {
+      let area;
+      const {location} = route;
+      if (location.length >= 4) // Find the location, 4th from the top hierarchy if it exists
+      {
+         area = location[3];
+      }
+      else // If the location doesn't have a hierarchy depth of 4, use the last available
+      {
+         area = location[location.length-1];
+      }
+
+      if (area in locations)
+      {
+         locations[area] += 1;
+
+      }
+      else
+      {
+         locations[area] = 1;
+      }
+   });
+   for (let location in locations)
+   {
+      locationsArr.push(`<li><a href='${location}'>${location}</a></li>`);
+   }
+   //routesArr[0] = routes.routes[0].name;
+   res.render('locationList', {
+      locationsList: `${locationsArr.join(``)}`
    });
 });
 
-app.get('/:route?', (req, res) => {
+app.get('/:location?', (req, res) => {
    // Fetch the available problems at the route location and link to each problem
    let problems = []; // Each problem should link to the problem page
-   res.render('route', {
-      route: `${req.params.route}`,
+   res.render('location', {
+      location: `${req.params.location}`,
       problemList: problems
    });
 });
 
-app.get('/:route/:problem', (req, res) => {
+app.get('/:location/:problem', (req, res) => {
    // Fetch all information for the problem and display the information with the route pug template
    res.render('problem', {
-      route: `<a href='/${req.params.route}'>${req.params.route}</a>`,
+      location: `<a href='/${req.params.location}'>${req.params.location}</a>`,
       problem: `${req.params.problem}`
       // Put other relevant information here
    });
